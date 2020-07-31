bee92f9e81e78f51cf24720f7d011ee81f85bf9e
Enrique Quijano <kike.qt@gmail.com>
2020-07-30 19:27:44 -0500

    Update signer.py
    Delegacion de funciones hacia locksmith

--- signer.py
+++ signer.py
@@ -1,4 +1,4 @@
-__version__ = "$Version: 0.0.1"
+__version__ = "$Version: 1.0.0"
 
 from base64 import b64encode
 from base64 import b64decode
@@ -9,60 +9,45 @@ from Crypto.Signature import PKCS1_v1_5
 from os.path import exists
 
 from license_verifier import License_Verifier
+from locksmith import Locksmith
 
 
 class Signer(License_Verifier):
     _cipher = None
+    _locksmith = None 
     
     def __init__(self, key_to_the_file, external_object=None):
         # We pass the object for communication with graphic interface
         self._external_object = external_object
         
-        self._config(key_to_the_file)
-        
-        self._cipher = AES.new(self._key_file.digest(),
-                               AES.MODE_OCB, nonce=self._nonce)
-        self.__generate_keys()
-        
-        self._cipher = AES.new(self._key_file.digest(),
-                               AES.MODE_OCB, nonce=self._nonce)
-        # Illustrative example, but you should enter the public and private variables directly in the self._public_key and self._private_key
-        self._load_key_files()
-        # I recommend this way
-        # self._public_key = b'Here you should enter the contents of the public key'
-        # self._private_key = b'Here you should enter the contents of the private key'
-        
-        self.__make_license_file()
-    
-    def _config(self, key_to_the_file):
         super()._config()
+
+        self._locksmith = Locksmith()
         
-        # Preparing keys
-        self._key_file = SHA256.new()
-        self._key_file.update(key_to_the_file)
-        self._nonce = self._key_file.digest()[:15]
+        # BEGIN Illustrative example, but you should enter the public and private
+        # variables directly in the self._public_key and self._private_key
+        if exists(self._public_key_file):
+            self._load_key_files()
+        
+        else:
+            self.__generate_keys(key_to_the_file)
+        # END Illustrative example
+        
+        # I recommend this way
+        # self._locksmith.set_public_key = b'-----BEGIN RSA PRIVATE KEY-----\n...'
+        # self._private_key = Here you should enter the contents of the crypt private key'
+        
+        self.__make_license_file(key_to_the_file)
     
-    def __generate_keys(self):
+    def __generate_keys(self, key_to_the_file):
         if not exists(self._public_key_file):
             self._add_2_status("Generating keys")
-            # Randomly generated key
-            key = RSA.generate(2048)
-            private_key = key.export_key()
-            public_key = key.publickey().export_key()
-            
-            with open(self._public_key_file, "wb") as file_out:
-                file_out.write(public_key)
-            
-            # We protect the private key
-            data = private_key
-            length = 16 - (len(data) % 16)
-            data += bytes([length])*length
-            
-            ciphertext = self._cipher.encrypt(data)
 
-            with open(self._private_key_file, "wb") as file_out:
-                file_out.write(ciphertext)
-                
+            self._locksmith.build_keys(key_to_the_file)
+            
+            self._locksmith.save_private_key(self._private_key_file)
+            self._locksmith.save_public_key(self._public_key_file)
+            
             self._add_2_status("\tSuccessfully")
     
     def _load_key_files(self):
@@ -70,24 +55,21 @@ class Signer(License_Verifier):
         
         self._add_2_status("Loading private key")
         
-        with open(self._private_key_file, "rb") as file:
-            self._private_key = file.read()
+        self._locksmith.load_private_key(self._private_key_file)
             
         self._add_2_status("\tSuccessfully")
     
-    def __make_license_file(self):
+    def __make_license_file(self, key_to_the_file):
         if not exists(self._license_file):
             self._add_2_status("Building license file")
-            data = self._cipher.decrypt(self._private_key)
-            data = data[: -data[-1]]
-            private_key = data
+            private_key = self._locksmith.decrypt_private_key(key_to_the_file)
             
             try:
-                self._private_key = private_key.decode()
-                
                 rsa_key = RSA.importKey(private_key)
                 signer = PKCS1_v1_5.new(rsa_key)
                 
+                # data = self._id_hardware.__str__()
+                # data = data.encode()
                 data = self._id_hardware.__str__().encode()
                 digest = SHA256.new(b64encode(data))
                 
@@ -109,12 +91,14 @@ class Signer(License_Verifier):
     
 
 if __name__ == '__main__':
-    # This section is for debugging only
-    # from os import system
-    # system('erase *.pem')
-    # system('erase *.lic')
+    # BEGIN Debug. This section is for debugging only
+    from os import system
+    system('cls')
+    system('erase *.pem')
+    system('erase *.lic')
+    # END Debug
     
     # You should enter this value in some other way
-    password = b'Replace this key'
+    password = 'Replace this key'
     
     license_maker = Signer(password)
